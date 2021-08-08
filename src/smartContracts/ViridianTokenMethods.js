267ac1937ca1459e891327ed32bf8d0d1254fb14
Blake Hatch <blakewihatch@gmail.com>
2021-08-08 19:32:47 -0400

    Added necessary transaction functions

---
+++ src/smartContracts/ViridianTokenMethods.js
@@ -0,0 +1,35 @@
+import config from "../local-dev-config";
+import vNFTJSON from "../abis/ViridianNFT.json";
+import Web3 from "web3";
+let web3 = new Web3(Web3.givenProvider || "HTTP://127.0.0.1:7545");
+
+export async function tokenURI(tokenId) {
+    const vNFTContractAddress = config.dev_contract_addresses.vnft_contract;
+
+    let vNFTABI = new web3.eth.Contract(vNFTJSON['abi'], vNFTContractAddress);
+    await console.log("ABIMETHODS: " + tokenId);
+    let nft = vNFTABI.methods.tokenURI(tokenId).call();
+
+    //alert(nft);
+
+    return nft;
+}
+
+export async function ownerOf(tokenId) {
+    const vNFTContractAddress = config.dev_contract_addresses.vnft_contract;
+
+    let vNFTABI = new web3.eth.Contract(vNFTJSON['abi'], vNFTContractAddress);
+    await console.log("ABIMETHODS: " + tokenId);
+    let owner = await vNFTABI.methods.ownerOf(tokenId).call();
+
+    //alert(nft);
+
+    return owner;
+}
+
+export async function setApprovalForAll(from, exchangeAddress) {
+    const vNFTContractAddress = config.dev_contract_addresses.vnft_contract;
+
+    let vNFTABI = new web3.eth.Contract(vNFTJSON['abi'], vNFTContractAddress);
+    await vNFTABI.methods.setApprovalForAll(exchangeAddress, true).send({from: from});
+}
