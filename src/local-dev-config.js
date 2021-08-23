21f0e4bf9c190184291b2adf6e1f788ff3f8375b
krishmoran <43583683+krishmoran@users.noreply.github.com>
2021-08-23 15:52:15 -0400

    possible gitignore fix

--- src/local-dev-config.js
+++ src/local-dev-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     dev_contract_addresses: {
-        vnft_contract: "0xB3f48f931Ba07a0C0dC39174B13c496644803e5f",
-        ve_contract: "0x85F960df1e248ba13186c357f152DFe00Ae674C0",
-        vt_contract: "0x54539ECb869D3F9F034885d67177eFd3ef89a982",
+        vnft_contract: "0xE7eda3431c2a9681414de9f08Df170fFD480972b",
+        ve_contract: "0x3C8d164213B843d45283dA16AD97D457A11Dc899",
+        vt_contract: "0x0c89F973c55B334660c5a1C6Da11D446cD3a1de4",
     },
 
     prod_contract_addresses: {
