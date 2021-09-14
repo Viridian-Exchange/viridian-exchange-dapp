7025d5d676a1aeef44b2395e7f1bb82b1f149457
Blake Hatch <blakewihatch@gmail.com>
2021-09-14 13:44:27 -0400

    Added more support for pack and card metadata

--- src/screens/Item/index.js
+++ src/screens/Item/index.js
@@ -5,19 +5,10 @@ import Users from "./Users";
 import Control from "./Control";
 import Options from "./Options";
 import { useLocation, withRouter } from 'react-router-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 const navLinks = ["Info", "Owners", "History", "Bids"];
-
-const categories = [
-  {
-    category: "black",
-    content: "Pokemon",
-  },
-  {
-    category: "purple",
-    content: "Graded",
-  },
-];
 
 const users = [
   {
@@ -26,17 +17,28 @@ const users = [
     avatar: "/images/content/avatar-2.jpg",
     reward: "/images/content/reward-1.svg",
   },
-  {
-    name: "Selina Mayert",
-    position: "Creator",
-    avatar: "/images/content/avatar-1.jpg",
-  },
 ];
 
 const Item = (props) => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [showCategories, setShowCategories] = useState(false);
   //const {passedState} = props.location.state
   const location = useLocation();
+
+  const categories = [
+    {
+      category: "black",
+      content: location.state.uri.type,
+    },
+    {
+      category: "gold",
+      content: location.state.uri.grade,
+    },
+    {
+      category: "purple",
+      content: location.state.uri.set,
+    },
+  ];
 
   return (
     <>
@@ -44,27 +46,58 @@ const Item = (props) => {
       <div className={cn("section", styles.section)}>
         <div className={cn("container", styles.container)}>
           <div className={styles.bg}>
-            <div className={styles.preview}>
-              <div className={styles.categories}>
-                {categories.map((x, index) => (
-                  <div
-                    className={cn(
-                      { "status-black": x.category === "black" },
-                      { "status-purple": x.category === "purple" },
-                      styles.category
-                    )}
-                    key={index}
-                  >
-                    {x.content}
-                  </div>
-                ))}
-              </div>
+            <div className={styles.preview} onMouseEnter={() => setShowCategories(true)}
+                 onMouseLeave={() => setShowCategories(false)}>
               {!location.state.isPack &&
-              <img
-                //srcSet="/images/content/item-pic@2x.jpg 2x"
-                src={location.state.uri.image}
-                alt="Item"
-              />}
+                <Carousel>
+                    <div>
+                      <div className={styles.categories} >
+                    {showCategories && categories.map((x, index) => (
+                        <div
+                            className={cn(
+                                { "status-black": x.category === "black" },
+                                { "status-purple": x.category === "purple" },
+                                { "status-gold": x.category === "gold" },
+                                { "status-blue": x.category === "blue" },
+                                styles.category
+                            )}
+                            key={index}
+                        >
+                          {x.content}
+                        </div>
+
+                    ))}
+                  </div>
+                  <img
+                      //srcSet="/images/content/item-pic@2x.jpg 2x"
+                      src={location.state.uri.image}
+                      alt="Item"
+                  />
+                  </div>
+                  <div>
+                    <div className={styles.categories}>
+                    {showCategories && categories.map((x, index) => (
+                        <div
+                            className={cn(
+                                { "status-black": x.category === "black" },
+                                { "status-purple": x.category === "purple" },
+                                { "status-gold": x.category === "gold" },
+                                { "status-blue": x.category === "blue" },
+                                styles.category
+                            )}
+                            key={index}
+                        >
+                          {x.content}
+                        </div>
+                    ))}
+                  </div>
+                  <img
+                      //srcSet="/images/content/item-pic@2x.jpg 2x"
+                      src={location.state.uri.imageBack}
+                      alt="Item2"
+                  />
+                  </div>
+                </Carousel>}
               {location.state.isPack &&
               <video autoPlay loop muted style={{maxWidth: '100ex'}}>
                 <source src={location.state.uri.image} type="video/mp4"/>
@@ -80,39 +113,40 @@ const Item = (props) => {
                 {location.state.price} VEXT
               </div>
               <div className={cn("status-stroke-black", styles.price)}>
-                $4,429.87
+                $0
               </div>
-              <div className={styles.counter}>10 in stock</div>
+              {/*<div className={styles.counter}>10 in stock</div>*/}
             </div>
-            <div className={styles.info}>
-              This NFT Card will give you Access to Special Airdrops. To learn
-              more about UI8 please visit{" "}
-              <a
-                href="https://ui8.net"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                https://ui8.net
-              </a>
-            </div>
-            <div className={styles.nav}>
-              {navLinks.map((x, index) => (
-                <button
-                  className={cn(
-                    { [styles.active]: index === activeIndex },
-                    styles.link
-                  )}
-                  onClick={() => setActiveIndex(index)}
-                  key={index}
-                >
-                  {x}
-                </button>
-              ))}
-            </div>
+            {/*<div className={styles.info}>*/}
+            {/*  This NFT Card will give you Access to Special Airdrops. To learn*/}
+            {/*  more about UI8 please visit{" "}*/}
+            {/*  <a*/}
+            {/*    href="https://ui8.net"*/}
+            {/*    target="_blank"*/}
+            {/*    rel="noopener noreferrer"*/}
+            {/*  >*/}
+            {/*    https://ui8.net*/}
+            {/*  </a>*/}
+            {/*</div>*/}
+
+            {/*<div className={styles.nav}>*/}
+            {/*  {navLinks.map((x, index) => (*/}
+            {/*    <button*/}
+            {/*      className={cn(*/}
+            {/*        { [styles.active]: index === activeIndex },*/}
+            {/*        styles.link*/}
+            {/*      )}*/}
+            {/*      onClick={() => setActiveIndex(index)}*/}
+            {/*      key={index}*/}
+            {/*    >*/}
+            {/*      {x}*/}
+            {/*    </button>*/}
+            {/*  ))}*/}
+            {/*</div>*/}
 
             {/*{JSON.stringify(isListing)}*/}
             <Users account={props.account} className={styles.users} items={users} owner={location.state.nftOwner} />
-            <Control price={location.state.price} className={styles.control} state={location.state} owner={location.state.nftOwner} account={props.account} isListing={location.state.isListing} />
+            <Control isPack={location.state.isPack} price={location.state.price} className={styles.control} state={location.state} owner={location.state.nftOwner} account={props.account} isListing={location.state.isListing} />
           </div>
         </div>
       </div>
