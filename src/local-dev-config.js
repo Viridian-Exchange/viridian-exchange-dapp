51423c3cfe75caf125e7eb1fd5559dd29e2b27ba
Blake Hatch <blakewihatch@gmail.com>
2021-09-29 22:51:41 -0400

    Fixed contract addresses

--- src/local-dev-config.js
+++ src/local-dev-config.js
@@ -1,10 +1,10 @@
 module.exports = {
     dev_contract_addresses: {
-        vnft_contract: "0x30E0C4219cDF325BaDCAD6e2CA9baB14e04E2b68",
-        ve_contract: "0xd4cBF814cC0c33342CE7bE96dC29E9Ba654de1ae",
-        vt_contract: "0x3C8d164213B843d45283dA16AD97D457A11Dc899",
-        vp_contract: "0x973Db0490D4de637bBaea07EB2065914BDb2C068",
-        vo_contract: "0x420C66006d99277F7ec17A6e4b025c8c1fAacbFa"
+        vnft_contract: "0x832222a8e6706b38013BdaC18BB60341EF327dE7",
+        ve_contract: "0xB83Ef24788f75338E5E3A9a6332630eA3286A0Db",
+        vt_contract: "0x85F960df1e248ba13186c357f152DFe00Ae674C0",
+        vp_contract: "0xCA45c787C5A0083908367Fd825Feb41455e48D78",
+        vo_contract: "0xeAABfaf18b60fEcc299631c77E2FBB03507cfF99"
     },
 
     prod_contract_addresses: {
