8bc0962e9d971e66ce80ca73d3125aa45ab651fd
Blake Hatch <blakewihatch@gmail.com>
2021-10-13 14:26:10 -0400

    Fixed offer alignment

--- src/local-dev-config.js
+++ src/local-dev-config.js
@@ -1,10 +1,10 @@
 module.exports = {
     dev_contract_addresses: {
-        vnft_contract: "0x26191666BFe79689578c5db2768ab9FBb366C472",
-        ve_contract: "0xcCF2738F4cf192B8DF0175Ce12ab510c5316003B",
-        vt_contract: "0xD9Eb9AbfA9aB461F1D8D708D8C9376186fC72898",
-        vp_contract: "0xfC34056796D871d7cD6248429422CCbca4B909dB",
-        vo_contract: "0xbd0D7271a8fcb4795C1BD4302541c7eC09153c12"
+        vnft_contract: "0x832222a8e6706b38013BdaC18BB60341EF327dE7",
+        ve_contract: "0xB83Ef24788f75338E5E3A9a6332630eA3286A0Db",
+        vt_contract: "0x85F960df1e248ba13186c357f152DFe00Ae674C0",
+        vp_contract: "0xCA45c787C5A0083908367Fd825Feb41455e48D78",
+        vo_contract: "0xeAABfaf18b60fEcc299631c77E2FBB03507cfF99"
     },
 
     prod_contract_addresses: {
