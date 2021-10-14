06993930d2aa7b073f446298916e5ec65a1c654f
krishmoran <43583683+krishmoran@users.noreply.github.com>
2021-10-13 21:53:10 -0400

    events working for offers, listings

--- src/local-dev-config.js
+++ src/local-dev-config.js
@@ -1,10 +1,10 @@
 module.exports = {
     dev_contract_addresses: {
-        vnft_contract: "0x26191666BFe79689578c5db2768ab9FBb366C472",
-        ve_contract: "0xcCF2738F4cf192B8DF0175Ce12ab510c5316003B",
-        vt_contract: "0xD9Eb9AbfA9aB461F1D8D708D8C9376186fC72898",
-        vp_contract: "0xfC34056796D871d7cD6248429422CCbca4B909dB",
-        vo_contract: "0xbd0D7271a8fcb4795C1BD4302541c7eC09153c12"
+        vnft_contract: "0x30E0C4219cDF325BaDCAD6e2CA9baB14e04E2b68",
+        ve_contract: "0xd4cBF814cC0c33342CE7bE96dC29E9Ba654de1ae",
+        vt_contract: "0x3C8d164213B843d45283dA16AD97D457A11Dc899",
+        vp_contract: "0x973Db0490D4de637bBaea07EB2065914BDb2C068",
+        vo_contract: "0x420C66006d99277F7ec17A6e4b025c8c1fAacbFa"
     },
 
     prod_contract_addresses: {
