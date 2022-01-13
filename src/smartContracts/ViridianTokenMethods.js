3d1109278797d1679680f4e87a8e5e72042871c1
Blake Hatch <blakewihatch@gmail.com>
2022-01-12 23:18:25 -0500

    Meta transactions enabled site-wide

--- src/smartContracts/ViridianTokenMethods.js
+++ src/smartContracts/ViridianTokenMethods.js
@@ -2,11 +2,11 @@ import config from "../local-dev-config";
 import vTJSON from "../abis/MetaTransactionTokenABI.json";
 import Web3 from "web3";
 import {Biconomy} from "@biconomy/mexa";
-//let web3 = new Web3(Web3.givenProvider || new Web3.providers.HttpProvider("https://polygon-mumbai.g.alchemy.com/v2/XvPpXkhm8UtkGw9b8tIMcR3vr1zTZd3b") || "HTTP://127.0.0.1:7545");
+let web3 = new Web3(Web3.givenProvider || new Web3.providers.HttpProvider("https://polygon-mumbai.g.alchemy.com/v2/XvPpXkhm8UtkGw9b8tIMcR3vr1zTZd3b") || "HTTP://127.0.0.1:7545");
 
-const biconomy = new Biconomy(Web3.givenProvider || new Web3.providers.HttpProvider( "https://polygon-mumbai.g.alchemy.com/v2/XvPpXkhm8UtkGw9b8tIMcR3vr1zTZd3b"),{apiKey: "O5RPM2y3A.bab3f010-10fb-47e4-8fa7-1efae42ab1b5", debug: true});
+//const biconomy = new Biconomy(Web3.givenProvider || new Web3.providers.HttpProvider( "https://polygon-mumbai.g.alchemy.com/v2/XvPpXkhm8UtkGw9b8tIMcR3vr1zTZd3b"),{apiKey: "O5RPM2y3A.bab3f010-10fb-47e4-8fa7-1efae42ab1b5", debug: true});
 
-let web3 = new Web3(biconomy);
+//let web3 = new Web3(biconomy);
 
 // biconomy.onEvent(biconomy.READY, () => {
 //     // Initialize your dapp here like getting user accounts etc
@@ -27,9 +27,11 @@ export async function approve(from, exchangeAddress, amount) {
     //TODO: Figure out why from is wrong
     alert(from);
 
-    alert(await vTABI.methods.allowance('0x9399BB24DBB5C4b782C70c2969F58716Ebbd6a3b', exchangeAddress).call());
+    alert(await vTABI.methods.allowance(from, exchangeAddress).call());
 
-    //let tx = await vTABI.methods.approve(exchangeAddress, '1000000000000000000000000000000000000000').send({from: from, signatureType: biconomy.EIP712_SIGN});
+    let approve_amount = '115792089237316195423570985008687907853269984665640564039457584007913129639935'; //(2^256 - 1 )
+
+    let tx = await vTABI.methods.approve(exchangeAddress, '115792089237316195423570985008687907853269984665640564039457584007913129639935').send({from: from});//, signatureType: biconomy.EIP712_SIGN});
 
     // await tx.on("transactionHash", function (hash) {
     //     console.log(`Transaction hash is ${hash}`);
@@ -40,5 +42,6 @@ export async function approve(from, exchangeAddress, amount) {
     //     //do something with transaction hash
     // });
 
+    await console.log(JSON.stringify(tx))
     //await console.log(JSON.stringify(tx))
 }
